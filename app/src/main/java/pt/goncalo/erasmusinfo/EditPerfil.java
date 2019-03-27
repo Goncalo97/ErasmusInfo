00dfba0d5f7866fc6fc1b99dcf61d21b433350b5
Gonçalo <Goncalo97@users.noreply.github.com>
2019-03-27 22:26:28 +0000

    Internacionalização; Validação da activity <EditPerfil>;

--- app/src/main/java/pt/goncalo/erasmusinfo/EditPerfil.java
+++ app/src/main/java/pt/goncalo/erasmusinfo/EditPerfil.java
@@ -2,13 +2,11 @@ package pt.goncalo.erasmusinfo;
 
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class EditPerfil extends AppCompatActivity {
@@ -24,6 +22,49 @@ public class EditPerfil extends AppCompatActivity {
         Toast.makeText(this, "{"+getString(R.string.edit_profile)+"}", Toast.LENGTH_LONG).show();
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public void saveProfile (View view) {
+
+        // get ID
+        EditText editProfileName = (EditText) findViewById(R.id.editTextViewProfileName);
+        EditText editProfileAge  = (EditText) findViewById(R.id.editTextViewProfileAge);
+
+        // get View data
+        String stringProfileName = editProfileName.getText().toString();
+        String stringProfileAge = editProfileAge.getText().toString();
+
+        if (stringProfileName.trim().length() == 0) {
+
+            // err Attribution
+            editProfileName.setError(getString(R.string.required_profile_name));
+
+            // focus on field
+            editProfileName.requestFocus();
+            return;
+        }
+
+        if (stringProfileAge.trim().length() == 0) {
+
+            // err Attribution
+            editProfileAge.setError(getString(R.string.required_profile_age));
+
+            // focus on field
+            editProfileAge.requestFocus();
+            return;
+        }
+
+        finish();
+
+        // getString of Resource @string/saved
+        Toast.makeText(this, "{"+getString(R.string.saved)+"}", Toast.LENGTH_SHORT).show();
+    }
+
+    public void cancelProfile (View view) {
+
+        // getString of Resource @string/saved
+        Toast.makeText(this, "{"+getString(R.string.canceled)+"}", Toast.LENGTH_SHORT).show();
+        finish();
     }
 
 }
