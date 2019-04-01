552e1dd9f45dee42fac8c36a456da27d6450e2fe
Gonçalo <17440801+Goncalo97@users.noreply.github.com>
2019-04-02 00:56:11 +0100

    Criar ProfileInsert Class

--- app/src/main/java/pt/goncalo/erasmusinfo/ProfileInsert.java
+++ app/src/main/java/pt/goncalo/erasmusinfo/ProfileInsert.java
@@ -1,16 +1,25 @@
 package pt.goncalo.erasmusinfo;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
 
 public class ProfileInsert extends AppCompatActivity {
+
+    EditText editProfileAge;
+    Calendar myCalendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,15 +28,88 @@ public class ProfileInsert extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        // getString of Resource @string/profile_insert
+        Toast.makeText(this, "{" + getString(R.string.profile_insert) + "}", Toast.LENGTH_LONG).show();
+
+        editProfileAge = (EditText) findViewById(R.id.editTextViewProfileAge);
+        myCalendar = Calendar.getInstance();
+
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel();
+            }
+
+        };
+
+        editProfileAge.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(ProfileInsert.this, date,
+                        myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
             }
         });
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void updateLabel() {
+        String myFormat = "dd/MM/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat,
+                new Locale("pt", "PT"));
+
+        editProfileAge.setText(sdf.format(myCalendar.getTime()));
+    }
+
+    public void saveProfile(View view) {
+
+        // get ID
+        EditText editProfileName = (EditText) findViewById(R.id.editTextViewProfileName);
+
+        // get View data
+        String stringProfileName = editProfileName.getText().toString();
+        String stringProfileAge = editProfileAge.getText().toString();
+
+        if (stringProfileName.trim().length() == 0) {
+
+            // err Attribution
+            editProfileName.setError(getString(R.string.required_profile_name));
+
+            // focus on field
+            editProfileName.requestFocus();
+            return;
+        }
+
+        if (stringProfileAge.trim().length() == 0) {
+
+            // err Attribution
+            editProfileAge.setError(getString(R.string.required_profile_age));
+
+            // focus on field
+            editProfileAge.requestFocus();
+            return;
+        }
+
+        finish();
+
+        // getString of Resource @string/saved
+        Toast.makeText(this, "{" + getString(R.string.saved) + "}", Toast.LENGTH_SHORT).show();
+
+    }
+
+    public void cancelProfile(View view) {
+
+        // getString of Resource @string/saved
+        Toast.makeText(this, "{" + getString(R.string.canceled) + "}", Toast.LENGTH_SHORT).show();
+        finish();
     }
 
 }
