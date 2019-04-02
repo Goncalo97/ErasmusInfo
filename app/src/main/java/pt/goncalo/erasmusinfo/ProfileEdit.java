c3d2f3f3d73c4f955200864c49b3414d0c6ad85d
Gonçalo <17440801+Goncalo97@users.noreply.github.com>
2019-04-02 18:19:26 +0100

    Formatar codigo da class ProfileEdit

--- app/src/main/java/pt/goncalo/erasmusinfo/ProfileEdit.java
+++ app/src/main/java/pt/goncalo/erasmusinfo/ProfileEdit.java
@@ -15,7 +15,6 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
 
-
 public class ProfileEdit extends AppCompatActivity {
 
     EditText editProfileAge;
@@ -28,19 +27,15 @@ public class ProfileEdit extends AppCompatActivity {
         setContentView(R.layout.activity_profile_edit);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         // getString of Resource @string/profile_edit
         Toast.makeText(this, "{" + getString(R.string.profile_edit) + "}", Toast.LENGTH_LONG).show();
-
         editProfileAge = (EditText) findViewById(R.id.editTextViewProfileAge);
         myCalendar = Calendar.getInstance();
-
         date = new DatePickerDialog.OnDateSetListener() {
 
             @Override
             public void onDateSet(DatePicker view, int year, int monthOfYear,
                                   int dayOfMonth) {
-
                 myCalendar.set(Calendar.YEAR, year);
                 myCalendar.set(Calendar.MONTH, monthOfYear);
                 myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
@@ -48,9 +43,7 @@ public class ProfileEdit extends AppCompatActivity {
             }
 
         };
-
         editProfileAge.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View v) {
                 new DatePickerDialog(ProfileEdit.this, date,
@@ -58,7 +51,6 @@ public class ProfileEdit extends AppCompatActivity {
                         myCalendar.get(Calendar.DAY_OF_MONTH)).show();
             }
         });
-
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
@@ -66,54 +58,43 @@ public class ProfileEdit extends AppCompatActivity {
         String myFormat = "dd/MM/yy"; //In which you need put here
         SimpleDateFormat sdf = new SimpleDateFormat(myFormat,
                 new Locale("pt", "PT"));
-
         editProfileAge.setText(sdf.format(myCalendar.getTime()));
     }
 
     public void saveProfile(View view) {
-
         // get ID
         EditText editProfileName = (EditText) findViewById(R.id.editTextViewProfileName);
-
         // get View data
         String stringProfileName = editProfileName.getText().toString();
         String stringProfileAge = editProfileAge.getText().toString();
 
         if (stringProfileName.trim().length() == 0) {
-
             // err Attribution
             editProfileName.setError(getString(R.string.required_profile_name));
-
             // focus on field
             editProfileName.requestFocus();
             return;
         }
 
         if (stringProfileAge.trim().length() == 0) {
-
             // err Attribution
             editProfileAge.setError(getString(R.string.required_profile_age));
-
             // focus on field
             editProfileAge.requestFocus();
-            if(editProfileAge.hasFocus())
-                new DatePickerDialog(ProfileEdit.this, date,
-                        myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
-                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            if(editProfileAge.hasFocus()) new DatePickerDialog(ProfileEdit.this, date,
+                    myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                    myCalendar.get(Calendar.DAY_OF_MONTH)).show();
 
             return;
         }
-
         // getString of Resource @string/saved
         Toast.makeText(this, "{" + getString(R.string.saved) + "}", Toast.LENGTH_SHORT).show();
         finish();
     }
 
     public void cancelProfile(View view) {
-
         // getString of Resource @string/saved
         Toast.makeText(this, "{" + getString(R.string.canceled) + "}", Toast.LENGTH_SHORT).show();
         finish();
     }
-
 }
