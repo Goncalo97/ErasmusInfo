727247dabdec76c57ba08acfba06b4f7e477e97f
Gonçalo <17440801+Goncalo97@users.noreply.github.com>
2019-04-07 22:20:15 +0100

    Adicionar funções para abrir activities (Ver, Inserir, Editar, Eliminar)

--- app/src/main/java/pt/goncalo/erasmusinfo/Contact.java
+++ app/src/main/java/pt/goncalo/erasmusinfo/Contact.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.view.View;
 import android.widget.Toast;
 
 public class Contact extends AppCompatActivity {
@@ -16,16 +17,31 @@ public class Contact extends AppCompatActivity {
         setContentView(R.layout.activity_contact);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        showMessageContactos();
+        Toast.makeText(this, "{"+ getString(R.string.contact)+"}", Toast.LENGTH_SHORT).show();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
-    private void showMessageContactos() {
+    public void openContactView(View view) {
 
-        Intent intent = getIntent();
-        String message = intent.getStringExtra(AppConsts.MESSAGE_CONTACTOS);
-        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(this, ContactView.class);
+        startActivity(intent);
     }
 
+    public void openContactInsert(View view) {
+
+        Intent intent = new Intent(this, ContactInsert.class);
+        startActivity(intent);
+    }
+
+    public void openContactEdit(View view) {
+
+        Intent intent = new Intent(this, ContactEdit.class);
+        startActivity(intent);
+    }
+
+    public void openContactDelete(View view) {
+
+        Intent intent = new Intent(this, ContactDelete.class);
+        startActivity(intent);
+    }
 }
