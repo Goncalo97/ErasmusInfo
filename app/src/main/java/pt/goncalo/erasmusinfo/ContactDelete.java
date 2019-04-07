db7989b7c9ed4b33971ce876f81c683ff1ce0e92
Gonçalo <17440801+Goncalo97@users.noreply.github.com>
2019-04-08 00:57:43 +0100

    Refatorar Contact Activities string.xml

--- app/src/main/java/pt/goncalo/erasmusinfo/ContactDelete.java
+++ app/src/main/java/pt/goncalo/erasmusinfo/ContactDelete.java
@@ -9,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.Toast;
 
 public class ContactDelete extends AppCompatActivity {
 
@@ -21,4 +22,16 @@ public class ContactDelete extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
+    public void profileDelete(View view) {
+
+        Toast.makeText(this, "{" + getString(R.string.profile_delete_toast_deleted) + "}", Toast.LENGTH_SHORT).show();
+        finish();
+
+    }
+
+    public void cancelProfile(View view) {
+
+        Toast.makeText(this, "{" + getString(R.string.profile_edit_toast_canceled) + "}", Toast.LENGTH_SHORT).show();
+        finish();
+    }
 }
