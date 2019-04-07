7b64d3aca00dda885f497df22c5bc0d85762ea4a
Gonçalo <17440801+Goncalo97@users.noreply.github.com>
2019-04-07 23:43:50 +0100

    Refatorar Contact Activities

--- app/src/main/java/pt/goncalo/erasmusinfo/ContactInsert.java
+++ app/src/main/java/pt/goncalo/erasmusinfo/ContactInsert.java
@@ -18,15 +18,6 @@ public class ContactInsert extends AppCompatActivity {
         setContentView(R.layout.activity_contact_insert);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
