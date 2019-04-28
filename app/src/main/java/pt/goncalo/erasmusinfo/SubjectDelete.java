77b2886c54e67dd0219d891cd3493f48d2fbfd98
Gonçalo <17440801+Goncalo97@users.noreply.github.com>
2019-04-29 00:41:45 +0100

    Criar SubjectDelete Class

---
+++ app/src/main/java/pt/goncalo/erasmusinfo/SubjectDelete.java
@@ -0,0 +1,24 @@
+package pt.goncalo.erasmusinfo;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+public class SubjectDelete extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subject_delete);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
