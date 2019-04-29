6eb6cb476ce0d6d8c68abafa3f87fadc63752724
Gonçalo <17440801+Goncalo97@users.noreply.github.com>
2019-04-29 06:47:20 +0100

    Adicionar Funções

--- app/src/main/java/pt/goncalo/erasmusinfo/CollegeDelete.java
+++ app/src/main/java/pt/goncalo/erasmusinfo/CollegeDelete.java
@@ -9,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.Toast;
 
 public class CollegeDelete extends AppCompatActivity {
 
@@ -21,4 +22,20 @@ public class CollegeDelete extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
+    public void collegeDelete(View view) {
+
+        Toast.makeText(this,
+                "{" + getString(R.string.college_delete_toast_deleted) + "}",
+                Toast.LENGTH_SHORT).show();
+        finish();
+
+    }
+
+    public void collegeCancel(View view) {
+
+        Toast.makeText(this,
+                "{" + getString(R.string.college_delete_toast_canceled) + "}",
+                Toast.LENGTH_SHORT).show();
+        finish();
+    }
 }
