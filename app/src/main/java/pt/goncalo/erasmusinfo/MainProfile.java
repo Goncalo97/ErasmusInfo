9e6f3e9e66d8b8cc816d29c428483b0091b2ce34
Gonçalo <17440801+Goncalo97@users.noreply.github.com>
2019-04-29 07:46:58 +0100

    Criar Class

---
+++ app/src/main/java/pt/goncalo/erasmusinfo/MainProfile.java
@@ -0,0 +1,29 @@
+package pt.goncalo.erasmusinfo;
+
+import java.util.Date;
+
+public class MainProfile {
+
+    public MainProfile() {
+
+    }
+
+    private String name;
+    private Date date;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
