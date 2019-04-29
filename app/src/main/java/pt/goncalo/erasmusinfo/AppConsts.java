3efcad78b6453a10c7b8ef44c1bbfe0d8b72ad2f
Gonçalo <17440801+Goncalo97@users.noreply.github.com>
2019-04-29 08:29:11 +0100

    Refatorar Tabelas

--- app/src/main/java/pt/goncalo/erasmusinfo/AppConsts.java
+++ app/src/main/java/pt/goncalo/erasmusinfo/AppConsts.java
@@ -3,6 +3,7 @@ package pt.goncalo.erasmusinfo;
 // Como fazer o = " pt.gon ... " automaticamente??
 class AppConsts {
 
+
     public static final String MESSAGE_PERFIL = "pt.goncalo.erasmusinfo.MESSAGE_PERFIL";
     public static final String MESSAGE_CONTACTOS = "pt.goncalo.erasmusinfo.MESSAGE_CONTACTOS";
     public static final String MESSAGE_UNIVERSIDADE = "pt.goncalo.erasmusinfo.MESSAGE_UNIVERSIDADE";
