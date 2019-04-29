71bed32ed67a591db28eb4221df610c02ab099e9
Gonçalo <17440801+Goncalo97@users.noreply.github.com>
2019-04-29 06:42:17 +0100

    Adicionar Funções

--- app/src/main/java/pt/goncalo/erasmusinfo/CollegeEdit.java
+++ app/src/main/java/pt/goncalo/erasmusinfo/CollegeEdit.java
@@ -9,6 +9,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class CollegeEdit extends AppCompatActivity {
 
@@ -20,5 +22,37 @@ public class CollegeEdit extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+    
+    public void collegeSave(View view) {
+        EditText collegeEditName = findViewById(R.id.editTextCollegeName);
+        EditText collegeEditCountry = findViewById(R.id.editTextCollegeCountry);
+        EditText collegeEditLocation = findViewById(R.id.editTextCollegeLocation);
 
+        if (collegeEditName.getText().toString().trim().length() == 0) {
+            collegeEditName.setError(getString(R.string.college_edit_textInputEditText_error_name));
+            collegeEditName.requestFocus();
+            return;
+        }
+        else if (collegeEditCountry.getText().toString().trim().length() == 0) {
+            collegeEditCountry.setError(getString(R.string.college_edit_textInputEditText_error_country));
+            collegeEditCountry.requestFocus();
+            return;
+        }
+        else if (collegeEditLocation.getText().toString().trim().length() == 0) {
+            collegeEditLocation.setError(getString(R.string.college_edit_textInputEditText_error_location));
+            collegeEditLocation.requestFocus();
+            return;
+        }
+        Toast.makeText(this,
+                "{" + getString(R.string.college_edit_toast_saved) + "}",
+                Toast.LENGTH_SHORT).show();
+        finish();
+    }
+
+    public void collegeCancel(View view) {
+        Toast.makeText(this,
+                "{" + getString(R.string.college_edit_toast_canceled) + "}",
+                Toast.LENGTH_SHORT).show();
+        finish();
+    }
 }
