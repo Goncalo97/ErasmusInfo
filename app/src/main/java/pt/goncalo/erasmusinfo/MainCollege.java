45363aca824c5ff446533ca37e70075a50a1cd45
Gonçalo <17440801+Goncalo97@users.noreply.github.com>
2019-04-29 07:54:43 +0100

    Refatorar Class

--- app/src/main/java/pt/goncalo/erasmusinfo/MainCollege.java
+++ app/src/main/java/pt/goncalo/erasmusinfo/MainCollege.java
@@ -1,15 +1,24 @@
 package pt.goncalo.erasmusinfo;
-
-import java.util.Date;
-
+8
 public class MainCollege {
 
     public MainCollege() {
 
     }
 
+    public MainCollege(String code, String name, String ects, String discEqual, String score) {
+        this.code = code;
+        this.name = name;
+        this.ects = ects;
+        this.discEqual = discEqual;
+        this.score = score;
+    }
+
+    private String code;
     private String name;
-    private Date date;
+    private String ects;
+    private String discEqual;
+    private String score;
 
     public String getName() {
         return name;
@@ -19,12 +28,47 @@ public class MainCollege {
         this.name = name;
     }
 
-    public Date getDate() {
-        return date;
+    public String getCode() {
+        return code;
     }
 
-    public void setDate(Date date) {
-        this.date = date;
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getEcts() {
+        return ects;
+    }
+
+    public void setEcts(String ects) {
+        this.ects = ects;
+    }
+
+    public String getDiscEqual() {
+        return discEqual;
+    }
+
+    public void setDiscEqual(String discEqual) {
+        this.discEqual = discEqual;
+    }
+
+    public String getScore() {
+        return score;
+    }
+
+    public void setScore(String score) {
+        this.score = score;
+    }
+
+    @Override
+    public String toString() {
+        return "MainCollege{" +
+                "code='" + code + '\'' +
+                ", name='" + name + '\'' +
+                ", ects='" + ects + '\'' +
+                ", discEqual='" + discEqual + '\'' +
+                ", score='" + score + '\'' +
+                '}';
     }
 }
 
