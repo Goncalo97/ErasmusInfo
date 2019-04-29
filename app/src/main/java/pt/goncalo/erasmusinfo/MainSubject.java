403221394375bde673afd33a2106a3b52b1bba21
Gonçalo <17440801+Goncalo97@users.noreply.github.com>
2019-04-29 07:58:29 +0100

    Refatorar Class

---
+++ app/src/main/java/pt/goncalo/erasmusinfo/MainSubject.java
@@ -0,0 +1,74 @@
+package pt.goncalo.erasmusinfo;
+
+public class MainSubject {
+
+
+    public MainSubject() {
+
+    }
+
+    public MainSubject(String code, String name, String ects, String discEqual, String score) {
+        this.code = code;
+        this.name = name;
+        this.ects = ects;
+        this.discEqual = discEqual;
+        this.score = score;
+    }
+
+    private String code;
+    private String name;
+    private String ects;
+    private String discEqual;
+    private String score;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getEcts() {
+        return ects;
+    }
+
+    public void setEcts(String ects) {
+        this.ects = ects;
+    }
+
+    public String getDiscEqual() {
+        return discEqual;
+    }
+
+    public void setDiscEqual(String discEqual) {
+        this.discEqual = discEqual;
+    }
+
+    public String getScore() {
+        return score;
+    }
+
+    public void setScore(String score) {
+        this.score = score;
+    }
+
+    @Override
+    public String toString() {
+        return "MainSubject{" +
+                "code='" + code + '\'' +
+                ", name='" + name + '\'' +
+                ", ects='" + ects + '\'' +
+                ", discEqual='" + discEqual + '\'' +
+                ", score='" + score + '\'' +
+                '}';
+    }
+}
