034dfbd98d405eecb32bfa21a87cc1e1c7ee9986
Gonçalo <17440801+Goncalo97@users.noreply.github.com>
2019-04-29 01:56:16 +0100

    Adicionar Funções

--- app/src/main/java/pt/goncalo/erasmusinfo/SubjectDelete.java
+++ app/src/main/java/pt/goncalo/erasmusinfo/SubjectDelete.java
@@ -9,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.Toast;
 
 public class SubjectDelete extends AppCompatActivity {
 
@@ -21,4 +22,20 @@ public class SubjectDelete extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
+    public void subjectDelete(View view) {
+
+        Toast.makeText(this,
+                "{" + getString(R.string.subject_delete_toast_deleted) + "}",
+                Toast.LENGTH_SHORT).show();
+        finish();
+
+    }
+
+    public void subjectCancel(View view) {
+
+        Toast.makeText(this,
+                "{" + getString(R.string.subject_delete_toast_canceled) + "}",
+                Toast.LENGTH_SHORT).show();
+        finish();
+    }
 }
