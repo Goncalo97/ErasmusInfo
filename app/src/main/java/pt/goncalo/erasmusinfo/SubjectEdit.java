8a41a07fe96e571dabf7ade192df3b74f17ecb64
Gonçalo <17440801+Goncalo97@users.noreply.github.com>
2019-04-29 01:35:26 +0100

    Adicionar Funções

--- app/src/main/java/pt/goncalo/erasmusinfo/SubjectEdit.java
+++ app/src/main/java/pt/goncalo/erasmusinfo/SubjectEdit.java
@@ -9,6 +9,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SubjectEdit extends AppCompatActivity {
 
@@ -19,5 +21,51 @@ public class SubjectEdit extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+    
+    public void subjectSave(View view) {
+        EditText subjectEditCode = findViewById(R.id.editTextSubjectCode);
+        EditText subjectEditName = findViewById(R.id.editTextSubjectName);
+        EditText subjectEditECTs = findViewById(R.id.editTextSubjectECTs);
+        EditText subjectEditDiscEqual = findViewById(R.id.editTextSubjectDiscEqual);
+        EditText subjectEditScore = findViewById(R.id.editTextSubjectScore);
+
+        if (subjectEditCode.getText().toString().trim().length() == 0) {
+            subjectEditCode.setError(getString(R.string.subject_edit_textInputEditText_error_code));
+            subjectEditCode.requestFocus();
+            return;
+        }
+        else if (subjectEditName.getText().toString().trim().length() == 0) {
+            subjectEditName.setError(getString(R.string.subject_edit_textInputEditText_error_name));
+            subjectEditName.requestFocus();
+            return;
+        }
+        else if (subjectEditECTs.getText().toString().trim().length() == 0) {
+            subjectEditECTs.setError(getString(R.string.subject_edit_textInputEditText_error_ects));
+            subjectEditECTs.requestFocus();
+            return;
+        }
+        else if (subjectEditDiscEqual.getText().toString().trim().length() == 0) {
+            subjectEditDiscEqual.setError(getString(R.string.subject_edit_textInputEditText_error_discEqual));
+            subjectEditDiscEqual.requestFocus();
+            return;
+        }
+        else if (subjectEditScore.getText().toString().trim().length() == 0) {
+            subjectEditScore.setError(getString(R.string.subject_edit_textInputEditText_error_score));
+            subjectEditScore.requestFocus();
+            return;
+        }
+
+        Toast.makeText(this,
+                "{" + getString(R.string.subject_edit_toast_saved) + "}",
+                Toast.LENGTH_SHORT).show();
+        finish();
+    }
+
+    public void subjectCancel(View view) {
+        Toast.makeText(this,
+                "{" + getString(R.string.subject_edit_toast_canceled) + "}",
+                Toast.LENGTH_SHORT).show();
+        finish();
     }
 }
