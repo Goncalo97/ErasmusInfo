7b69cd3299c88ef319c5e6b9560356b74536d2c5
Gonçalo <17440801+Goncalo97@users.noreply.github.com>
2019-04-29 06:44:41 +0100

    Adicionar Funções

--- app/src/main/java/pt/goncalo/erasmusinfo/CollegeInsert.java
+++ app/src/main/java/pt/goncalo/erasmusinfo/CollegeInsert.java
@@ -9,6 +9,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class CollegeInsert extends AppCompatActivity {
 
@@ -20,5 +22,36 @@ public class CollegeInsert extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+    
+    public void collegeSave(View view) {
+        EditText collegeInsertName = findViewById(R.id.editTextCollegeName);
+        EditText collegeInsertCountry = findViewById(R.id.editTextCollegeCountry);
+        EditText collegeInsertLocation = findViewById(R.id.editTextCollegeLocation);
+        if (collegeInsertName.getText().toString().trim().length() == 0) {
+            collegeInsertName.setError(getString(R.string.college_edit_textInputEditText_error_name));
+            collegeInsertName.requestFocus();
+            return;
+        }
+        else if (collegeInsertCountry.getText().toString().trim().length() == 0) {
+            collegeInsertCountry.setError(getString(R.string.college_edit_textInputEditText_error_country));
+            collegeInsertCountry.requestFocus();
+            return;
+        }
+        else if (collegeInsertLocation.getText().toString().trim().length() == 0) {
+            collegeInsertLocation.setError(getString(R.string.college_edit_textInputEditText_error_location));
+            collegeInsertLocation.requestFocus();
+            return;
+        }
+        Toast.makeText(this,
+                "{" + getString(R.string.college_edit_toast_saved) + "}",
+                Toast.LENGTH_SHORT).show();
+        finish();
+    }
 
+    public void collegeCancel(View view) {
+        Toast.makeText(this,
+                "{" + getString(R.string.college_edit_toast_canceled) + "}",
+                Toast.LENGTH_SHORT).show();
+        finish();
+    }
 }
