45ceb3aaff23807fe75bb29c351d9db7ee1e4898
Gonçalo <17440801+Goncalo97@users.noreply.github.com>
2019-05-12 17:06:53 +0100

    Adicionar class para representar a tabela College da base de dados Adicionar código SQL para criar a tabela College Adicionar constantes para o nome da tabela e dos campos da tabela College Implementar operações CRUD na tabela College Definir uma constante com todas as colunas das tabela College

---
+++ app/src/main/java/pt/goncalo/erasmusinfo/BdTableCollege.java
@@ -0,0 +1,48 @@
+package pt.goncalo.erasmusinfo;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.provider.BaseColumns;
+
+public class BdTableCollege implements BaseColumns {
+    public static final String TABLE_NAME = "contact";
+    public static final String FIELD_NAME = "name";
+    public static final String FIELD_COUNTRY = "country";
+    public static final String FIELD_LOCATION = "location";
+    private SQLiteDatabase db;
+
+    public static final String[] ALL_COLUMNS = new String[] {_ID, FIELD_NAME, FIELD_COUNTRY, FIELD_LOCATION };
+
+    public BdTableCollege(SQLiteDatabase db) {
+        this.db = db;
+    }
+
+    public void create() {
+        db.execSQL(
+                "CREATE TABLE " + TABLE_NAME + " (" +
+                        _ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                        FIELD_NAME + " TEXT NOT NULL," +
+                        FIELD_COUNTRY + " TEXT NOT NULL," +
+                        FIELD_LOCATION + " TEXT NOT NULL" +
+                        ")"
+        );
+    }
+
+    public Cursor query(String[] columns, String selection, String[] selectionArgs, String groupBy,
+                        String having, String orderBy) {
+        return db.query(TABLE_NAME, columns, selection, selectionArgs, groupBy, having, orderBy);
+    }
+
+    public long insert(ContentValues values) {
+        return db.insert(TABLE_NAME, null, values);
+    }
+
+    public int 	update(ContentValues values, String whereClause, String[] whereArgs) {
+        return db.update(TABLE_NAME, values, whereClause, whereArgs);
+    }
+
+    public int delete(String whereClause, String[] whereArgs) {
+        return db.delete(TABLE_NAME, whereClause, whereArgs);
+    }
+}
