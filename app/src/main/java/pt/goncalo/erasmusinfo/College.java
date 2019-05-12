c9cb4c3106ff06cfc168d19083741a1c4a5dc117
Gonçalo <17440801+Goncalo97@users.noreply.github.com>
2019-05-12 21:25:16 +0100

    Refatorar classe College (Business Model) Adicionar função getContentValues à classe College Adicionar função fromCursor à classe College

--- app/src/main/java/pt/goncalo/erasmusinfo/College.java
+++ app/src/main/java/pt/goncalo/erasmusinfo/College.java
@@ -1,6 +1,8 @@
 package pt.goncalo.erasmusinfo;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +12,61 @@ import android.view.View;
 import android.widget.Toast;
 
 public class College extends AppCompatActivity {
+
+    private long id;
+    private String name;
+    private String country;
+    private String location;
+
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public String getCountry() {
+        return country;
+    }
+    public String getLocation() {
+        return location;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setCountry(String country) {
+        this.country = country;
+    }
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public ContentValues getContentValues() {
+        ContentValues values = new ContentValues();
+        values.put(BdTableCollege.FIELD_NAME, name);
+        values.put(BdTableCollege.FIELD_COUNTRY, country);
+        values.put(BdTableCollege.FIELD_LOCATION, location);
+        return values;
+    }
+
+    public static College fromCursor(Cursor cursor) {
+        long id = cursor.getLong(cursor.getColumnIndex(BdTableCollege._ID));
+        String name = cursor.getString(cursor.getColumnIndex(BdTableCollege.FIELD_NAME));
+        String country = cursor.getString(cursor.getColumnIndex(BdTableCollege.FIELD_COUNTRY));
+        String location = cursor.getString(cursor.getColumnIndex(BdTableCollege.FIELD_LOCATION));
+
+        College college = new College();
+        college.setId(id);
+        college.setName(name);
+        college.setCountry(country);
+        college.setCountry(location);
+
+        return college;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
