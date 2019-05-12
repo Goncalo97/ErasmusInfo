9936fe3c73ba560ee9cb8dbb71731126f4cc6958
Gonçalo <17440801+Goncalo97@users.noreply.github.com>
2019-05-12 16:45:32 +0100

    Adicionar class para representar a tabela Contact da base de dados Adicionar código SQL para criar a tabela Contact Adicionar constantes para o nome da tabela e dos campos da tabela Contact Implementar operações CRUD na tabela Contact

---
+++ app/src/main/java/pt/goncalo/erasmusinfo/BdTableContact.java
@@ -0,0 +1,46 @@
+package pt.goncalo.erasmusinfo;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.provider.BaseColumns;
+
+public class BdTableContact implements BaseColumns {
+    public static final String TABLE_NAME = "contact";
+    public static final String FIELD_NAME = "name";
+    public static final String FIELD_NUMBER = "number";
+    private SQLiteDatabase db;
+
+    public static final String[] ALL_COLUMNS = new String[] {_ID, FIELD_NAME, FIELD_NUMBER };
+
+    public BdTableContact(SQLiteDatabase db) {
+        this.db = db;
+    }
+
+    public void create() {
+        db.execSQL(
+                "CREATE TABLE " + TABLE_NAME + " (" +
+                        _ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                        FIELD_NAME + " TEXT NOT NULL," +
+                        FIELD_NUMBER + " TEXT NOT NULL" +
+                        ")"
+        );
+    }
+
+    public Cursor query(String[] columns, String selection, String[] selectionArgs, String groupBy,
+                        String having, String orderBy) {
+        return db.query(TABLE_NAME, columns, selection, selectionArgs, groupBy, having, orderBy);
+    }
+
+    public long insert(ContentValues values) {
+        return db.insert(TABLE_NAME, null, values);
+    }
+
+    public int 	update(ContentValues values, String whereClause, String[] whereArgs) {
+        return db.update(TABLE_NAME, values, whereClause, whereArgs);
+    }
+
+    public int delete(String whereClause, String[] whereArgs) {
+        return db.delete(TABLE_NAME, whereClause, whereArgs);
+    }
+}
